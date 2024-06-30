3a24bf4e2b625ca36801a9316d5a85194a880864
Tolea01 <anatoliecurbat@gmail.com>
2024-06-30 22:51:03 +0300

    add cors options

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,16 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigModule } from '@nestjs/config';
 
 export default class AppConfig {
   public static setupConfigModule(): ReturnType<typeof ConfigModule.forRoot> {
     return ConfigModule.forRoot({ isGlobal: true });
+  }
+
+  public static getCorsOptions(): CorsOptions {
+    return {
+      origin: ['http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory, NestApplication } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import buildAppiDocs from './docs/swagger.builder';
+import AppConfig from './config/app.config';
 
 async function bootstrap(): Promise<void> {
   const app: NestApplication = await NestFactory.create(AppModule);
+
+  app.enableCors(AppConfig.getCorsOptions());
 
   buildAppiDocs<NestApplication>(app);
 
