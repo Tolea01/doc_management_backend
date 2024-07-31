9651bfe75e6f716fe86c7a8300cef3e30e14052a
Tolea01 <anatoliecurbat@gmail.com>
2024-07-31 15:01:31 +0300

    add lang property on translateMessage function

--- src/app/utils/translateMessage.ts
+++ src/app/utils/translateMessage.ts
@@ -1,9 +1,12 @@
-import { I18nService } from 'nestjs-i18n';
+import { I18nService, I18nContext } from 'nestjs-i18n';
 
 export const translateMessage = async (
   i18nService: I18nService,
   i18nMessage: string,
   args?: Record<string, any>,
 ): Promise<string> => {
-  return i18nService.translate(i18nMessage, { args });
+  return i18nService.translate(i18nMessage, {
+    lang: I18nContext.current().lang,
+    args,
+  });
 };
