b12548d290939977aaddcc546aaf95fcc9b854e2
Tolea01 <anatoliecurbat@gmail.com>
2024-11-29 14:18:03 +0200

    import modules to internal_document module

--- src/app/modules/internal_documents/internal_documents.module.ts
+++ src/app/modules/internal_documents/internal_documents.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
-import { InternalDocumentsService } from './internal_documents.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileManagementModule } from '../file_management/file_management.module';
+import { PersonModule } from '../person/person.module';
+import { UserModule } from '../user/user.module';
+import { InternalDocument } from './entities/internal_document.entity';
 import { InternalDocumentsController } from './internal_documents.controller';
+import { InternalDocumentsService } from './internal_documents.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([InternalDocument]),
+    UserModule,
+    PersonModule,
+    FileManagementModule,
+  ],
   controllers: [InternalDocumentsController],
   providers: [InternalDocumentsService],
 })
