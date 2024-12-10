3b5db6e701f06a68137585a97dd02255c88a7ee8
Tolea01 <anatoliecurbat@gmail.com>
2024-12-10 15:27:08 +0200

    user-seeder hash password

--- src/seeds/seeders/interfaces/user.data.interface.ts
+++ src/seeds/seeders/interfaces/user.data.interface.ts
@@ -3,7 +3,7 @@ import { UserRole } from 'app/modules/user/roles/role.enum';
 export default interface IUserData {
   name: string;
   surname: string;
-  password: string;
+  password: any;
   role: UserRole;
   photo: string;
   phone_number: string;
--- src/seeds/seeders/user.seeder.ts
+++ src/seeds/seeders/user.seeder.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'app/modules/user/entities/user.entity';
 import { UserRole } from 'app/modules/user/roles/role.enum';
+import { hash } from 'argon2';
 import { Repository } from 'typeorm';
 import IUserData from './interfaces/user.data.interface';
 
@@ -14,15 +15,17 @@ export class UserSeeder {
   ) {}
 
   async seed(): Promise<void> {
-    const data: IUserData[] = Array.from({ length: 20 }, () => ({
-      name: faker.person.firstName(),
-      surname: faker.person.lastName(),
-      password: 'password',
-      role: faker.helpers.arrayElement(Object.values(UserRole)),
-      photo: faker.image.avatar(),
-      phone_number: faker.phone.number().slice(0, 15),
-      email_address: faker.internet.email(),
-    }));
+    const data: IUserData[] = await Promise.all(
+      Array.from({ length: 20 }, async () => ({
+        name: faker.person.firstName(),
+        surname: faker.person.lastName(),
+        password: await hash('password'),
+        role: faker.helpers.arrayElement(Object.values(UserRole)),
+        photo: faker.image.avatar(),
+        phone_number: faker.phone.number().slice(0, 15),
+        email_address: faker.internet.email(),
+      })),
+    );
 
     for (const userData of data) {
       const userExist: User | undefined = await this.userRepository.findOneBy({
