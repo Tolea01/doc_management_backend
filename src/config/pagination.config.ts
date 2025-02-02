38bd235df3c4c4719850dd33762e53112090f4d6
Tolea01 <anatoliecurbat@gmail.com>
2025-02-02 21:14:14 +0200

    default limit pagination config

--- src/config/pagination.config.ts
+++ src/config/pagination.config.ts
@@ -2,7 +2,7 @@ import IpaginatorConfig from 'app/common/interfaces/IpaginatorConfig.interface';
 
 const paginationConfig: IpaginatorConfig = {
   page: 1,
-  limit: 10,
+  limit: 25,
   sortOrder: 'ASC',
   sortColumn: 'id',
 };
